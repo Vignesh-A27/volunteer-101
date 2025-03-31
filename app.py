0165170a46ca6bd8ecba861440bdb3eed74c2a02
Vignesh A <152258055+Vignesh-A27@users.noreply.github.com>
2025-03-31 19:38:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,40 @@ video_html = f"""
 # Inject HTML into Streamlit
 st.markdown(video_html, unsafe_allow_html=True)
 
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 # Main Content
 st.markdown('<div class="main-container">', unsafe_allow_html=True)
 
